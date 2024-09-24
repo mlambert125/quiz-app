5f73a248cc6c8a2279dd4c2f6df74e59028a72ec
Michael Lambert <mlambert125@live.com>
2024-09-23 20:29:26 -0400

    fixing base url

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://mlambert125.github.io/quiz-app/dist/',
+  base: '/quiz-app/dist/',
   plugins: [vue()],
   resolve: {
     alias: {
