2cf38c0b003b39e31691a2c06dc32dd525097099
Michael Lambert <mlambert125@live.com>
2024-09-23 20:26:33 -0400

    set the base url

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  base: 'https://mlambert125.github.io/quiz-app/dist/',
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
