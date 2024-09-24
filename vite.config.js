03ec3e7789b0054167f63f49fe9b2fa2712a2b96
Michael Lambert <mlambert125@live.com>
2024-09-23 20:39:51 -0400

    fixed config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/quiz-app/dist/',
+  base: '/quiz-app',
   plugins: [vue()],
   resolve: {
     alias: {
